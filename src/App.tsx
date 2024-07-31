a935ff0d0910a632b44799fc1ca0c8a89c4f0313
Pierre Oudin <pierre.oudin76@outlook.fr>
2024-08-01 00:00:40 +0200

    first card design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import { AppBar, Avatar, Box, Card, CardContent, CardMedia, Container, Tooltip, Typography } from '@mui/material'
+import React from 'react'
 
 function App() {
 
   return (
     <>
-      <p>Test</p>      
+    <AppBar position='static'>
+      <Typography variant='h6'>PlayState</Typography>
+    </AppBar>
+    <br/>
+    <Container maxWidth="sm">
+        <GameCard />
+      </Container>
     </>
   )
 }
 
+function GameCard() {
+  return (
+    <Tooltip title={<React.Fragment><Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}><Avatar src='src/assets/pngwing.com.png'/><Typography variant='caption'>Assassin's Creed Shadow</Typography></Box></React.Fragment>}>
+      <Box sx={{backgroundImage: `url("https://full.life/media/magefan_blog/Assassin_s_Creed_Shadows.jpg")`, backgroundSize: 'cover', backgroundRepeat: "no-repeat", backgroundPosition: 'center', width: 600, height: 200, position: 'relative'}}>
+      <Box sx={{position: 'absolute', backgroundColor: 'green', width: 50, height: 50, bottom:0, right:0, textAlign: 'center', }}>
+        <Typography sx={{}}>86</Typography>
+      </Box>
+    </Box>
+    </Tooltip>
+  )
+}
+
 export default App
+
+
